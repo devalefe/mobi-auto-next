c18d0778ecabb867e940e1b3ff6bccdb8d944661
devalefe <dev.alefe@gmail.com>
2022-07-16 22:56:42 -0300

    refactor: tranform MyDocument in _document to class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,21 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-const MyDocument = () => {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="stylesheet" href="global.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="stylesheet" href="global.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
-
-export default MyDocument;
